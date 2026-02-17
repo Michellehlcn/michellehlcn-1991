dd58cb561fea275fab2d86a71a1addb88955c045
Michellehlcn <michellehlcn.au@gmail.com>
2026-02-17 18:55:32 +1100

    add

--- webpage/src/components/_Header.tsx
+++ webpage/src/components/_Header.tsx
@@ -11,15 +11,15 @@ export function _Header() {
                 <div className="@container flex flex-1 justify-start pl-8"></div>
                 <div>
                     <nav className='flex items-center gap-5 max-md:hidden lg:gap-6'>
-                        <Link to="/artwork"><button className='hover:text-indigo-600 font-mono text-[0.8125rem]/6 font-medium tracking-widest text-pretty uppercase text-fuchsia-500 dark:text-fuchsia-400'>Artwork</button></Link>
+                        <Link to="/artwork"><button className='cursor-pointer hover:text-indigo-600 font-mono text-[0.8125rem]/6 font-medium tracking-widest text-pretty uppercase text-fuchsia-500 dark:text-fuchsia-400'>Artwork</button></Link>
                         <div className="h-6 w-px bg-gray-950/10 dark:bg-white/10"></div>
-                        <Link to="/#aboutme"><button  id="_aboutme" className='hover:text-indigo-600 text-s/6 text-gray-950 dark:text-white'>About</button></Link>
-                        <Link to="/#blog"><button id="_blog" className='hover:text-indigo-600 text-s/6 text-gray-950 dark:text-white'>Blog</button></Link>
-                        <Link to="/#projects"><button id="_projects" className='hover:text-indigo-600 text-s/6 text-gray-950 dark:text-white'>Projects</button></Link>
+                        <Link to="/#aboutme"><button  id="_aboutme" className='cursor-pointer hover:text-indigo-600 text-s/6 text-gray-950 dark:text-white'>About</button></Link>
+                        <Link to="/#blog"><button id="_blog" className='cursor-pointer hover:text-indigo-600 text-s/6 text-gray-950 dark:text-white'>Blog</button></Link>
+                        <Link to="/#projects"><button id="_projects" className='cursor-pointer hover:text-indigo-600 text-s/6 text-gray-950 dark:text-white'>Projects</button></Link>
                           
                         {/* <a href="#expertise" className='hover:text-indigo-600 text-s/6 text-gray-950 dark:text-white'>Expertise</a>
                         <a href="#cv" className='hover:text-indigo-600 text-s/6 text-gray-950 dark:text-white'>CV</a> */}
-                        <Link to="/#contact"><button id="_contact" className='text-s/6 text-gray-950 dark:text-white rounded-full bg-gray-950 px-2.5 py-0.5 text-sm/6 font-medium text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-950 dark:bg-gray-700 dark:text-white dark:focus-visible:outline-white'>Contact</button></Link>
+                        <Link to="/#contact"><button id="_contact" className='cursor-pointer text-s/6 text-gray-950 dark:text-white rounded-full bg-gray-950 px-2.5 py-0.5 text-sm/6 font-medium text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-950 dark:bg-gray-700 dark:text-white dark:focus-visible:outline-white'>Contact</button></Link>
             
                     </nav>
                 </div>
